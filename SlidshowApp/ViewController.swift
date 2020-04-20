fa5377d2416e5f9318e50729f9215f32c7016ea9
yu.i <vamos.ishida@icloud.com>
2020-04-21 04:32:37 +0900

    進む、戻るを配列と関数にて実装した。

--- SlidshowApp/ViewController.swift
+++ SlidshowApp/ViewController.swift
@@ -10,39 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var num: Int = 1
+    
+/*   var num: Int = 1
+      
+ //タイマー
+    var timer: Timer!
+    //タイマー用の時間の変数
+    var timer_sec:Float = 0
+    
+    @objc func updateTimer(_ timer:Timer){
+        self.timer_sec = 2.0
+        self.imageView.image =
+        }
+*/
+    let imgArray = ["pic1","pic2","pic3"]
+    var i:Int = 0
+    func imgfunc(img: Int){
+        let image = imgArray[img]
+        imageView.image = UIImage(named: image)
+    }
     
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func next(_ sender: Any) {
 
-      if num == 1 {
-        imageView.image = UIImage(named: "pic2")
-            num += 1
-        }else if num == 2{
-        imageView.image = UIImage(named: "pic3")
-            num += 1
+        if i <= (imgArray.count - 1){
+            imgfunc(img: i)
+             i += 1
         }else{
-        imageView.image = UIImage(named: "pic1")
-            num = 1
+            i = 0
+            imgfunc(img: i)
+            i += 1
         }
     }
     @IBAction func back(_ sender: Any) {
+        if i != 0{
+            i -= 1
+            imgfunc(img: i)
+        }else{
+            i = (imgArray.count - 1)
+            imgfunc(img: i)
+               }
+    }
 
-        if num == 1 {
-          imageView.image = UIImage(named: "pic3")
-              num = 3
-          }else if num == 2{
-          imageView.image = UIImage(named: "pic1")
-              num -= 1
-          }else{
-          imageView.image = UIImage(named: "pic2")
-              num -= 1
-          }
-    }
     @IBAction func start_stop(_ sender: Any) {
-        imageView.image = UIImage.animatedImageNamed("pic1", duration: 2.0)
-    }
+     //   imageView.image = UIImage.animatedImageNamed("pic1", duration: 2.0)
+/*        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+*/
+ }
     
     
     override func viewDidLoad() {
@@ -50,6 +65,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
       //  imageView.image = UIImage(named: "pic1")
     //    imageView.image = [UIImage imagedNamed: @"pictuer1.jpeg"]
+
     }
 
 
