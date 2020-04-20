53f0024e24f06f2ae203645b2ee9a373b678df8c
yu.i <vamos.ishida@icloud.com>
2020-04-20 18:11:38 +0900

    進む、戻るまで完成

--- SlidshowApp/ViewController.swift
+++ SlidshowApp/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var num: Int = 1
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func next(_ sender: Any) {
 
+      if num == 1 {
+        imageView.image = UIImage(named: "pic2")
+            num += 1
+        }else if num == 2{
+        imageView.image = UIImage(named: "pic3")
+            num += 1
+        }else{
+        imageView.image = UIImage(named: "pic1")
+            num = 1
+        }
+    }
+    @IBAction func back(_ sender: Any) {
+
+        if num == 1 {
+          imageView.image = UIImage(named: "pic3")
+              num = 3
+          }else if num == 2{
+          imageView.image = UIImage(named: "pic1")
+              num -= 1
+          }else{
+          imageView.image = UIImage(named: "pic2")
+              num -= 1
+          }
+    }
+    @IBAction func start_stop(_ sender: Any) {
+        imageView.image = UIImage.animatedImageNamed("pic1", duration: 2.0)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      //  imageView.image = UIImage(named: "pic1")
+    //    imageView.image = [UIImage imagedNamed: @"pictuer1.jpeg"]
     }
 
 
